7d681f37e9f4e7fcfa0cf932f41365bd0fe2bcd1
RJ Nowling <rnowling@gmail.com>
2023-01-05 11:50:16 -0600

    Swap DataFrame examples

--- src/main/scala/DataFrameExample1.scala
+++ src/main/scala/DataFrameExample1.scala
@@ -25,99 +25,42 @@ object DataFrameExample1 {
     // by providing an encoder
     import spark.implicits._
 
-    // This is a DataFrame, which is really a type alias
-    // for Dataset[Row]
-    // A Row is a trait (interface)
-    val df = spark.read
-      // CSV File has a header row which specifies the column names
-      .option("header", true)
-      // infer the types of the columns
-      .option("inferSchema", true)
-      // perform the actual read
-      .csv("Sacramentorealestatetransactions.csv")
-
-    val nRows = df.count()
-
-    println()
-    println(s"The data set has ${nRows} rows.")
-    println()
-
-    println("The schema is:")
-    df.printSchema()
-    println()
-
-    println("The first few rows are:")
+    val simpleData = Seq(("James", "Sales", "NY", 90000, 34, 10000),
+      ("Michael", "Sales", "NY", 86000, 56, 20000),
+      ("Robert", "Sales", "CA", 81000, 30, 23000),
+      ("Maria", "Finance", "CA", 90000, 24, 23000),
+      ("Raman", "Finance", "CA", 99000, 40, 24000),
+      ("Scott", "Finance", "NY", 83000, 36, 19000),
+      ("Jen", "Finance", "NY", 79000, 53, 15000),
+      ("Jeff", "Marketing", "CA", 80000, 25, 18000),
+      ("Kumar", "Marketing", "NY", 91000, 50, 21000)
+    )
+    var df = simpleData.toDF("employee_name", "department", "state", "salary", "age", "bonus")
     df.show()
+
+    df.printSchema()
+
+    // convert types
+    df = df.withColumn("salary", df("salary").cast("double"))
+    df = df.withColumn("bonus", df("bonus").cast("double"))
+
+    df.printSchema()
+
+    // computation on a column
+    val bonusPercent = df("bonus") / df("salary") * 100
+    df = df.withColumn("bonus_percent_salary", bonusPercent)
+
+    df.show()
+
+    df.filter(df("salary") >= 90000).show()
+
     println()
 
-    // short the first and last 5 values for each column
-    for(columnName <- df.columns) {
-      println(s"${columnName} column")
-      df.select(df(columnName))
-        .orderBy(df(columnName))
-        .show(5)
+    df.filter(!(df("salary") >= 90000)).show()
 
-      df.select(df(columnName))
-        .orderBy(df(columnName).desc)
-        .show(5)
-
-      println()
-    }
-
-    // We should see that there are rows with:
-    // 0 sq__ft
-    // 0 beds
-    // 0 baths
-    // "Unkown" (misspelled) type
-    // prices on the order of thousands
-    // let's figure out how many there are of each
-
-    val sqFt0Rows = df.filter("sq__ft = 0").count()
-    println(s"There are ${sqFt0Rows} rows with 0 sq ft")
     println()
 
-    val beds0Rows = df.filter("beds = 0").count()
-    println(s"There are ${beds0Rows} rows with 0 beds")
-    println()
-
-    val baths0Rows = df.filter("baths = 0").count()
-    println(s"There are ${baths0Rows} rows with 0 baths")
-    println()
-
-    val unknownTypeRows = df.filter("type = 'Unkown'").count()
-    println(s"There are ${unknownTypeRows} rows with unknown types")
-    println()
-
-    val prices = df.select("price").map(row => row.getInt(0))
-    val nDigits = prices.map(p => p.toString.length)
-
-    // print histogram of prices
-    nDigits.groupByKey(digits => digits)
-      .reduceGroups((currentSum, _) => currentSum + 1)
-      .toDF("NumberOfDigits", "Count")
-      .orderBy("NumberOfDigits")
-      .show()
-    println()
-
-    // We want to remove rows with outlier values
-    val noOutliersDF = df.filter("price >= 100000")
-      .filter("sq__ft > 0")
-      .filter("baths > 0")
-      .filter("beds > 0")
-      .filter("type <> 'Unkown'")
-
-    val countRemaining = noOutliersDF.count()
-
-    println(s"${countRemaining} of ${nRows} rows remain after filtering")
-
-    // parquet keeps column types
-    // by default, Spark will refuse to overwrite
-    // files.  Override this behavior so we can
-    // re-run the program without manually deleting
-    // the output
-    noOutliersDF.write
-      .mode("overwrite")
-      .parquet("cleaned")
+    df.filter(df("department") === "Finance").show()
 
     spark.stop()
   }
