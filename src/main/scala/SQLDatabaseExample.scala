b817c4297dd270aba753f40cd22085ea4f3761eb
RJ Nowling <rnowling@gmail.com>
2023-01-05 13:36:19 -0600

    Add postgres example

--- src/main/scala/SQLDatabaseExample.scala
+++ src/main/scala/SQLDatabaseExample.scala
@@ -1,3 +1,40 @@
-object SQLDatabaseExample {
+import org.apache.spark.sql.SparkSession
 
+import java.util.Properties
+
+object SQLDatabaseExample {
+  def main(args: Array[String]): Unit = {
+    // The Spark system is started by creating a SparkSession
+    // object.  This object is used to create and register
+    // all distributed collections.
+    val spark = SparkSession
+      .builder
+      // set number of cores to use in []
+      .master("local[4]")
+      .appName("DataFrameExample")
+      .getOrCreate()
+
+    // disable INFO log output from this point forward
+    spark.sparkContext.setLogLevel("ERROR")
+
+    // this is needed to allow the map function below
+    // by providing an encoder
+    import spark.implicits._
+
+    val url = "jdbc:postgresql://127.0.0.1:5432/spam_classification"
+    val tableName = "labeled_emails"
+    val props = new Properties()
+    props.setProperty("user", "postgres")
+    props.setProperty("password", "admin")
+
+    val tableDf = spark.read
+      .jdbc(url, tableName, props)
+
+    tableDf.printSchema()
+
+    tableDf.show()
+
+    spark.stop()
+
+  }
 }
